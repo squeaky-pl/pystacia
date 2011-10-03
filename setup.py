7ce93e9a0b0828b90449d677a88227e87f18551a
squeaky <none@none>
2011-10-04 00:03:54 +0200

    more packaging stuff
    --HG--
    rename : tinyimg/CHANGES => CHANGES
    rename : tinyimg/TODO => TODO

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(
     name='tinyimg',
     description='Python raster imaging library',
     author = 'Paweł Piotr Przeradowski',
+    author_email = 'przeradowski@gmail.com',
     url = 'https://bitbucket.org/squeaky/tinyimg',
     version='0.1dev',
-    packages=['tinyimg',],
+    packages=['tinyimg', 'tinyimg.api', 'tinyimg.tests', 'tinyimg.api.tests'],
     license='MIT License',
     long_description=open('README').read(),
 )
