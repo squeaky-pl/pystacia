5d0fa10c75a6b6384dc89938607b75b03acbbdc6
squeaky <none@none>
2011-10-03 22:33:05 +0200

    setup.py, license, ignore

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(
+    name='tinyimg',
+    description='Python raster imaging library',
+    author = 'Paweł Piotr Przeradowski',
+    url = 'https://bitbucket.org/squeaky/tinyimg',
+    version='0.1dev',
+    packages=['tinyimg',],
+    license='MIT License',
+    long_description=open('README').read(),
+)
