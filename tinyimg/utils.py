cd16c41afac861515454cbbd93dad421578fb286
squeaky <none@none>
2011-10-04 23:47:16 +0200

    py25 compat

--- tinyimg/utils.py
+++ tinyimg/utils.py
@@ -2,8 +2,12 @@ class TinyException(Exception): pass
 
 def only_live(f):
     def wrapper(obj, *args, **kw):
-        if obj.closed: raise TinyException('{0} already closed'.format(obj.__class__))
+        if obj.closed:
+            template = formattable('{0} already closed')
+            raise TinyException(template.format(obj.__class__))
         
         return f(obj, *args, **kw)
         
     return wrapper
+
+from tinyimg.compat import formattable
