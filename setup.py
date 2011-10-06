864417bbbfb7777d3ac9f7d524d0585ae153d822
squeaky <none@none>
2011-10-06 17:14:33 +0200

    include lena in package_data

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ try: format
 except NameError:
     install_requires.append('StringFormat')
 
-
 setup(
     name='tinyimg',
     description='Python raster imaging library',
@@ -17,6 +16,7 @@ setup(
     url = 'https://bitbucket.org/squeaky/tinyimg',
     version='0.1dev',
     packages=['tinyimg', 'tinyimg.api', 'tinyimg.tests', 'tinyimg.api.tests'],
+    package_data={'tinyimg': ['lena.ycbcr.bz2']},
     license='MIT License',
     long_description=open('README').read(),
     install_requires=install_requires
