b9eb26369a3dfa5bb0899e42240c1b48b7dea8ea
squeaky <none@none>
2012-05-19 14:48:44 +0200

    Simplify memoized a little bit
    --HG--
    branch : bridge-remove

--- pystacia/util.py
+++ pystacia/util.py
@@ -17,6 +17,10 @@ def chainable(f, obj, *args, **kw):
     return obj
 
 
+# memoized cache
+__cache = {}
+
+
 @decorator
 def memoized(f, *args, **kw):
     """Decorator that caches a function's return value each time it is called.
@@ -25,31 +29,25 @@ def memoized(f, *args, **kw):
     not re-evaluated. This decorator performs proper synchronization to make it
     thread-safe.
     """
-    if not hasattr(memoized, '__cache'):
-        with __lock:
-            if not hasattr(memoized, '__cache'):
-                memoized.__cache = {}
-    
-    cache = memoized.__cache
     key = f, args, frozenset(kw.items())
-    if key not in cache:
+    if key not in __cache:
         with __lock:
-            if key not in cache:
-                cache[key] = {'lock': RLock()}
+            if key not in __cache:
+                __cache[key] = {'lock': RLock()}
                 
-    key_cache = cache[key]
+    key_cache = __cache[key]
     if 'value' not in key_cache:
-        info = key[0].__name__, key[1]
-        msg = formattable('Memoizing {0} args={1}').format(*info)
-        logger.debug(msg)
-        
         with key_cache['lock']:
             if 'value' not in key_cache:
+                info = key[0].__name__, key[1]
+                msg = formattable('Memoizing {0} args={1}').format(*info)
+                logger.debug(msg)
+                
                 result = f(*args, **kw)
-                if 'value' not in key_cache:
-                    key_cache['value'] = result
-                    msg = formattable('Memoized {0} args={1}').format(*info)
-                    logger.debug(msg)
+                
+                key_cache['value'] = result
+                msg = formattable('Memoized {0} args={1}').format(*info)
+                logger.debug(msg)
             
     return key_cache['value']
 
