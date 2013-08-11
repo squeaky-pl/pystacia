b6e3683a5eb469a2d11878af318913e51c2acdbd
Paweł Piotr Przeradowski <przeradowski@gmail.com>
2013-08-11 21:55:56 +0200

    reduction

--- tool/process.py
+++ tool/process.py
@@ -5,6 +5,8 @@ from glob import glob
 from os.path import dirname, abspath, join, basename, splitext, exists
 from subprocess import check_call, check_output
 from re import compile
+from pprint import pprint
+
 
 here = dirname(abspath(__file__))
 typedef_tmpl = 'typedef\s+enum\s*\{([^\}]+)\}\s*'
@@ -74,6 +76,35 @@ def process(directory):
     return result
 
 
+def reduce_(data):
+    for key, values in data.items():
+        print(key.upper() + '---------------------------')
+        seen = {}
+        for i, value in enumerate(values):
+            copied = value.copy()
+            version = copied.pop('_version')
+            if copied == seen:
+                values[i] = None
+            else:
+                print(' version ' + str(version))
+                copied_set = set(copied.items())
+                seen_set = set(seen.items())
+                added = dict(copied_set - seen_set)
+                removed = dict(seen_set - copied_set)
+
+                if added:
+                    print(' + ' + str(added))
+                if removed:
+                    print(' - ' + str(removed))
+
+            seen = copied
+
+        data[key] = filter(None, values)
+
+
+min_version = (6, 5)
+
+
 def main():
     downloads = join(here, 'downloads')
     archives = glob(join(downloads, '*.tar.bz2'))
@@ -81,7 +112,13 @@ def main():
     directories = []
     for archive in archives:
         directory, _ = splitext(splitext(archive)[0])
-        directories.append(directory)
+
+        version_str = basename(directory).split('-', 1)[1].replace('-', '.')
+        version = tuple(int(v) for v in version_str.split('.'))
+        if version < min_version:
+            continue
+
+        directories.append((version, directory))
 
         if exists(directory):
             continue
@@ -89,10 +126,20 @@ def main():
         print('Unpacking ' + basename(archive))
         check_call(['tar', '-C', downloads, '-xf', archive])
 
-    for directory in directories:
-        process(directory)
+    data = dict((k, []) for k in enums)
+    for version, directory in sorted(directories):
+        result = process(directory)
 
-    return directories
+        for name, values in result.items():
+            values['_version'] = version[:-1]
+            data[name].append(values)
+
+    reduce_(data)
+
+    with open(join(here, 'data.py'), 'w') as f:
+        pprint(data, f)
+
+    return data
 
 
 if __name__ == '__main__':
